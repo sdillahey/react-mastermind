6b951950bd4048483028649cc895597551fffd4d
Stacy Dillahey <stacy.dillahey@gmail.com>
2017-05-23 11:56:06 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <header className="App-header">React Mastermind</header>
+
+    );
+  }
+}
+
+export default App;
